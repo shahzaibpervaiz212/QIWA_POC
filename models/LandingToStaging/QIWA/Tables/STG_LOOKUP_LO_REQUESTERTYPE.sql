d0469caa0d09e9071f63358b7070ee70f0d4d9e0
SHAHZAIB PERVAIZ <sh.pervaiz-c@takamol.com.sa>
2024-08-15 09:23:08 +0000

    first table deploy

--- models/LandingToStaging/QIWA/Tables/STG_LOOKUP_LO_REQUESTERTYPE.sql
+++ models/LandingToStaging/QIWA/Tables/STG_LOOKUP_LO_REQUESTERTYPE.sql
@@ -4,11 +4,5 @@
 
 ) }}
 
-with source_data as (
 
-    select * from {{ ref('vw_LOOKUP_LO_REQUESTERTYPE') }}
-
-),
-
-select *
-from source_data
+select * from {{ ref('VW_LOOKUP_LO_REQUESTERTYPE') }}
