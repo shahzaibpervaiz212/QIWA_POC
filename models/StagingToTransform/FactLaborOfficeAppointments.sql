3762caa955736d5978b1264479b54c089b41850d
mazz majid <m.abdelmgid@takamol.sa>
2024-08-27 08:57:34 +0000

    commit

---
+++ models/StagingToTransform/FactLaborOfficeAppointments.sql
@@ -0,0 +1,52 @@
+{{
+    config(
+        materialized="table",
+    )
+}}
+
+with
+    factlaborofficeappointments as (
+
+        select
+
+            id as appointmentid,
+            officeid,
+            laborofficeid,
+            sequencenumber,
+            establishmentid,
+            clientserviceid,
+            statusid,
+            requesteridno,
+            requestername,
+            requesteruserid,
+            appointmentdate,
+            appointmenttime,
+            rejectiondescription,
+            mwtransactionid,
+            creationdate,
+            lastmodifieddate,
+            regionid,
+            operatoridno,
+            channelid,
+            updatereasonid,
+            fk_requestertypeid,
+            fk_subserviceid,
+            status4date,
+            status6date,
+            --realsessiontime,
+            visitreasonid,
+            CASE WHEN ISNULL(visitreasonid,1) = 1 THEN N''حضوري'' ELSE N''افتراضي'' END AS AppointmentTypeAr,
+            CASE WHEN ISNULL(visitreasonid,1) = 1 THEN 'Physical'  ELSE 'Virtual' END AS AppointmentTypeEn ,
+            substring(realsessiontime, CHARINDEX('minutes',realsessiontime) + len(' minutes') ,(CHARINDEX(' seconds',realsessiontime))  - (CHARINDEX('minutes',realsessiontime) + len(' minutes'))) RealSessionTime, 
+            weblink,
+            meetingid
+
+
+
+        from {{ ref("VW_LABOROFFICEAPPOINTMENT") }} 
+      
+
+    )
+
+select *
+from factlaborofficeappointments
